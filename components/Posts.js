69567863f86768e097de23c9f727ef743962cf9e
Deleanu Narcis Florin <deleanunarcisflorin97@gmail.com>
2022-09-20 18:52:04 +0300

    create the header and image sections of the post component

--- components/Posts.js
+++ components/Posts.js
@@ -6,14 +6,14 @@ export default function Posts() {
         {
             id:"1",
             username:"Narcis",
-            userImg:"http://www.jennexplores.com/wp-content/uploads/2015/09/Instagram_logo_black.png",
+            userImg:"https://scontent.fcra2-1.fna.fbcdn.net/v/t1.6435-9/161054004_3380644385374879_2393323840128663172_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=nCEBwgVhMLIAX-UyI1R&_nc_ht=scontent.fcra2-1.fna&oh=00_AT_taiCD1JK_yUpibqTcxMyiTnQTPU-0hF8mIdEMbBpgeQ&oe=6351067D",
             img: "https://images.unsplash.com/photo-1663615280810-81f2f47f3b23?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw4fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60",
             caption: "Love this place"
         },
         {
             id:"2",
             username:"Florin",
-            userImg:"http://www.jennexplores.com/wp-content/uploads/2015/09/Instagram_logo_black.png",
+            userImg:"https://scontent.fcra2-1.fna.fbcdn.net/v/t1.6435-9/161054004_3380644385374879_2393323840128663172_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=nCEBwgVhMLIAX-UyI1R&_nc_ht=scontent.fcra2-1.fna&oh=00_AT_taiCD1JK_yUpibqTcxMyiTnQTPU-0hF8mIdEMbBpgeQ&oe=6351067D",
             img: "https://images.unsplash.com/photo-1663592151816-b30533dc08ec?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyMnx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60",
             caption: "Smoke the food"
         }
