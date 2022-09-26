34025049d2cf9d65fb6387e29b93a004bc34f75b
Deleanu Narcis Florin <deleanunarcisflorin97@gmail.com>
2022-09-26 16:44:59 +0300

    add the secret

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,6 +1,5 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
-
 export default NextAuth({
   // Configure one or more authentication providers
   providers: [
@@ -14,7 +13,6 @@ export default NextAuth({
   pages: {
     signIn: "/auth/signin",
   },
-
   callbacks: {
     async session({ session, token, user }) {
       session.user.username = session.user.name
